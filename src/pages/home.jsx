9c535c3972279f5b12763aac54a1d47738f257a7
dhandapani.d@tvsnext.com <dhandapani.d@tvsnext.com>
2024-06-07 14:08:07 +0530

    Login pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import '../css/home.css'
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import '../css/home.css';
+import image from '../assets/village.jpg'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
 
   return (
     <div>
       <h1>Home page</h1>
+      <div className="container">
+        <p>Welcome!!</p>
+        <img href='#' atl="images"src={image}></img>
+        <div/>
+        <button onClick={()=>navigate('/signin')}> Enter</button>
+        <span></span>
+        <div/>
+
+      </div>
     </div>
   );
 };
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import '../css/signin.css'
 const SignIn = () => {
   return (
-    <div>
-        <h1>Login page</h1>
-      
+    <div className="Login_form">
+      <h1>Login page</h1>
+        <label for="username">User Name</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Your user name.."
+        />
+
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Your password.."
+        />
+        <div className="buttons">
+        <button className="center-button" type="submit" value="Submit"> login </button>
+        <button className="center-button" type="submit" value="Submit"> signup </button>
+        </div>
     </div>
   );
 };
